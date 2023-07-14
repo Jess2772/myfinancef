04348525865f15076efbb485b31305e5672af797
Jesse Li <jesse.li286@gmail.com>
2023-07-14 19:45:25 -0400

    help

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  // baseURL: "http://127.0.0.1:8000"
+  //baseURL: "http://127.0.0.1:8000"
   baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com"
 });
 
@@ -66,9 +66,6 @@ function App() {
         {
           email: email,
           password: password
-        },
-        {
-          headers: corsHeaders
         }
       ).then(function(res) {
         setCurrentUser(true);
