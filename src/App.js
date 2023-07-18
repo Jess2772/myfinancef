e3deeea8df79413c5cea12927b9548c6f7d426f4
Jesse Li <jesse.li286@gmail.com>
2023-07-18 18:00:44 -0400

    named import link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TypeAnimation } from 'react-type-animation';
 
 
