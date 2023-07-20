92489bd800ce7239648b2633b02900b27dcc875f
Jesse Li <jesse.li286@gmail.com>
2023-07-19 23:00:14 -0400

    gt cookie

--- src/apis/Client.js
+++ src/apis/Client.js
@@ -5,8 +5,8 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-    //baseURL: "http://127.0.0.1:8000",
-    baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com/"
+    baseURL: "http://127.0.0.1:8000",
+    //baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com/"
   });
   
 export default client;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,30 @@
 import {useEffect, useState} from 'react';
 import client from '../apis/Client'
 import axios from 'axios';
+import jQuery from 'jquery'
 function Welcome() {
     const [data, setData] = useState([]);
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
     const getData = async () => {
-        const { data } = await client.get(`api/user`, {withCredentials: true});
+        const { data } = await client.get(`api/user`, {headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        }});
         setData(data);
     };
 
