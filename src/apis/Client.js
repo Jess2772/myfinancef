2b73c73c132c6233bde6d05161385d32a2cfa898
Jesse Li <jesse.li286@gmail.com>
2023-07-20 22:40:27 -0400

    change baseurl

--- src/apis/Client.js
+++ src/apis/Client.js
@@ -6,7 +6,8 @@ axios.defaults.withCredentials = true;
 
 const client = axios.create({
     //baseURL: "http://127.0.0.1:8000",
-    baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com",
+    //baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com",
+    baseUrl: "https://107.22.57.98:443",
     withCredentials: true,
     xsrfHeaderName: 'X-CSRFToken',
     xsrfCookieName: 'xsrftoken',
