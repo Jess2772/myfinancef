a8c8ce9c7961840611e665e7c316f7f9d2c1996a
Jesse Li <jesse.li286@gmail.com>
2023-07-20 22:23:56 -0400

    test

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,6 +2,9 @@ import {useEffect, useState} from 'react';
 import client from '../apis/Client'
 import axios from 'axios';
 import jQuery from 'jquery'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 function Welcome() {
     const [data, setData] = useState([]);
     function getCookie(name) {
@@ -19,7 +22,6 @@ function Welcome() {
         return cookieValue;
     }
     var csrftoken = getCookie('csrftoken');
-    console.log(csrftoken)
     const getData = async () => {
         const { data } = await client.get(`api/user`, {headers: {
             'Accept': 'application/json',
@@ -32,9 +34,27 @@ function Welcome() {
     useEffect(() => {
         getData();
       }, []);   
+    
+    console.log(data)
 
-    return <div>{JSON.stringify(data)}</div>;
-
+    return (
+        <div>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Link to="/" style={{ textDecoration: 'none' }}><Navbar.Brand>MyFinance</Navbar.Brand></Link>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">            
+                        <Navbar.Text>
+                        <Link to="/" className="btn btn-outline-dark">Settings</Link>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <h1 className="center">Welcome back, {data.user.username} </h1>
+            <h1 className="center">I'd like to....</h1>
+            <h1 className="center">Put buttons that lead to the other functionality</h1>
+        </div>
+    )
 }
 
 export default Welcome;
