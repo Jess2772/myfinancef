7c7da6474a73418d360dbcd4d0e2e59f7fd2dafe
Jesse Li <jesse.li286@gmail.com>
2023-07-28 14:06:55 -0400

    datagrid filtering/data types

--- src/pages/Spendings.js
+++ src/pages/Spendings.js
@@ -1,5 +1,5 @@
 import Navigation from "../components/Navigation";
-import { Row, Col, Form, Button, InputGroup } from 'react-bootstrap'
+import { Row, Col, Form, InputGroup } from 'react-bootstrap'
 import jwt_decode from "jwt-decode"
 import client from "../apis/Client";
 import { useState, useEffect } from "react";
@@ -10,6 +10,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
 import FirstPageIcon from '@mui/icons-material/FirstPage';
@@ -27,8 +28,8 @@ const columns = [
     { field: 'transaction_id', headerName: 'transaction_id', width: 250, headerClassName: 'style--header'},
     { field: 'merchant', headerName: 'Merchant', width: 250, headerClassName: 'style--header'},
     { field: 'category', headerName: 'Category', width: 250, headerClassName: 'style--header'},
-    { field: 'amount', headerName: 'Amount ($)', type: 'number', width: 250, headerClassName: 'style--header'},
-    { field: 'transaction_date', headerName: 'Date', width: 250, headerClassName: 'style--header'},
+    { field: 'amount', headerName: 'Amount ($)', type: 'number', valueGetter: ({ value }) => value && value.toFixed(2), width: 250, headerClassName: 'style--header'},
+    { field: 'transaction_date', headerName: 'Date', width: 250, type: 'date', valueGetter: ({ value }) => value && new Date(value.split('-')), headerClassName: 'style--header'},
     { field: 'pymt_method_full', headerName: 'Payment Method', width: 250, headerClassName: 'style--header'},
 ]
 
@@ -64,16 +65,20 @@ function Spendings() {
     return (
         <div >
             <Navigation></Navigation>
-            <h2 style={{display: 'flex', margin: 'auto', justifyContent:'center', alignItems:'center'}}>Your spending</h2>
+            <h2 style={{display: 'flex', margin: 'auto', justifyContent:'center', alignItems:'center'}}>Overall spending</h2>
+            <div className="center mt-5 mb-5">
+                    <Link style={{ textDecoration: 'none'}} to="/transactions">
+                        <Button variant="contained" size="large" className="center me-2"> View monthly spending by category </Button>
+                    </Link>
+                    <Link style={{ textDecoration: 'none'}} to="/spendings">
+                        <Button variant="contained" size="large" className="center me-2"> Month to day </Button>
+                    </Link>
+                    <Link style={{ textDecoration: 'none'}} to="/budget">
+                        <Button variant="contained" size="large" className="center me-2"> Projections </Button>
+                    </Link>
+            </div>
             <TableContainer style={{display: 'flex', margin: 'auto', justifyContent:'center', alignItems:'center'}} sx={{ maxWidth: 1500 }} component={Paper}>
                 <DataGrid
-                //  sx={{
-                //     height: 300,
-                //     
-                //     '& .super-app-theme--header': {
-                //       backgroundColor: 'rgba(255, 7, 0, 0.55)',
-                //     },
-                //   }}
                 sx={{
                     minHeight: 635,
                     boxShadow: 2,
@@ -95,7 +100,6 @@ function Spendings() {
                     }}
                     pageSizeOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                     disableRowSelectionOnClick
-                    
                 />
             </TableContainer>
 
