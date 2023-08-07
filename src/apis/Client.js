fe251a3e4992e6fba6186465c7c8f97b5632b44e
Jesse Li <jesse.li286@gmail.com>
2023-08-07 12:45:41 -0400

    url

--- src/apis/Client.js
+++ src/apis/Client.js
@@ -3,8 +3,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-    baseURL: "http://127.0.0.1:8000",
-    //baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com",
+    //baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://myfinancejb-2225ee8966e8.herokuapp.com",
     withCredentials: true,
   });
   
